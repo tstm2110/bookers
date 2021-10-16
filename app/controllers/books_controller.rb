f82c5ea5d1f54924befaa79a04b2695fc841f126
tstm2110 <ts2021ik06@gmail.com>
2021-10-16 11:38:08 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,24 +19,27 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
   end
    def update
+       @books = Book.all
     book = Book.find(params[:id])
     if  book.update(book_params)
     redirect_to book_path(book)
     else
+      render "index"
     end
   end
  
   def create
+    @books = Book.all
     # １. データを新規登録するためのインスタンス作成
     @book = Book.new(book_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     if  @book.save
     # ３. トップ画面へリダイレクト
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), success: '登録ができました'
       # 詳細画面へリダイレクト
     else
-      
-      redirect_to book_path
+      render "index"
+     
     end
   end
   private
